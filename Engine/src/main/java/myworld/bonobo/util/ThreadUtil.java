a0921d0223031bed893a4a6588c69b283427e112
danielperano <51095634+danielperano@users.noreply.github.com>
2022-06-01 18:56:11 -0700

    Replace thread parking with sleep()
    On Windows, the previous thread parking mechanism often caused the elapsed sleep phases to be roughly double the time requested because of some odd interaction with the Windows scheduler. Thread.sleep() does not exhibit this behavior and performs just as well on Linux.

--- Engine/src/main/java/myworld/bonobo/util/ThreadUtil.java
+++ Engine/src/main/java/myworld/bonobo/util/ThreadUtil.java
@@ -2,15 +2,17 @@ package myworld.bonobo.util;
 
 import myworld.bonobo.time.Timer;
 
-import java.util.concurrent.locks.LockSupport;
-
 public class ThreadUtil {
 
     public static void precisionSleep(Timer timer, long millis){
         long elapsed = 0;
         long lastTime = timer.currentMillis();
         while(elapsed < millis){
-            LockSupport.parkNanos(TimeUtil.millisToNanos(millis - elapsed));
+            try{
+                Thread.sleep(millis - elapsed);
+            }catch(InterruptedException e){
+                Thread.currentThread().interrupt();
+            }
             elapsed = timer.currentMillis() - lastTime;
         }
     }
