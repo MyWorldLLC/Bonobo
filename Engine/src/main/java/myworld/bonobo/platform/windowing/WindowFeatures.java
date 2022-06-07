367a67fc88fbee8ac6a7f375ca79288d4f9b6fbe
Daniel Perano <51095634+danielperano@users.noreply.github.com>
2022-06-06 17:27:11 -0700

    Support borderless windows

--- Engine/src/main/java/myworld/bonobo/platform/windowing/WindowFeatures.java
+++ Engine/src/main/java/myworld/bonobo/platform/windowing/WindowFeatures.java
@@ -1,8 +1,10 @@
 package myworld.bonobo.platform.windowing;
 
-public record WindowFeatures(String title, int width, int height, boolean startVisible) {
+public record WindowFeatures(String title, int width, int height, DisplayState state) {
+
+    public record DisplayState(boolean startVisible, boolean borderless){}
 
     public WindowFeatures(String title, int width, int height){
-        this(title, width, height, true);
+        this(title, width, height, new DisplayState(true, false));
     }
 }
